0ac454cfa6c1d1ddb3722320e9d4659859069ebc
ValeriaDiaz <valeriadiazbel@gmail.com>
2026-07-15 18:11:10 -0500

    commit

--- contador.js
+++ contador.js
@@ -1,9 +1,14 @@
-let cuenta = 0
+let cuenta = 0 
 const numero = document.getElementById("numero")
 
 function actualizarContador (){
 numero.textContent = cuenta ;
 
+if (cuenta >= 10){
+    numero.style.color = "brown"
+} else {
+    numero.style.color = "black"
+}
 }
 
 function incrementar(){
